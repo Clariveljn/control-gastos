46a22657da4dde81218201b9d06e28e6f645e604
Clariveljn <cjeldres5@gmail.com>
2022-12-27 10:36:53 -0300

    Editar Gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import Header from './components/Header'
 import Modal from './components/Modal';
 import ListadoGastos from './components/ListadoGastos';
 import { generarId } from './helpers';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+
 function App() {
+  
+  const [gastos, setGastos] = useState([])
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
@@ -12,21 +15,37 @@ function App() {
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
 
-  const [gastos, setGastos] = useState([])
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0) {
+      setModal(true)
+  
+      setTimeout(() => {
+        setAnimarModal(true)
+      },500);
+    }
+  }, [gastoEditar])
 
   const handleNuevoGasto = () => {
     setModal(true)
+    setGastoEditar({})
 
     setTimeout(() => {
       setAnimarModal(true)
-
-    },500)
+    },500);
   }
 
   const guardarGasto = gasto => {
-    gasto.id = generarId();
+    if(gasto.id) {
+      const gastosActualizados = gastos.map( gastoState => gastoState.id === gasto.id ? gasto : gastoState)
+      setGastos(gastosActualizados);
+    }else {
+         gasto.id = generarId();
     gasto.fecha = Date.now();
     setGastos([...gastos, gasto])
+    }
+ 
 
     setAnimarModal(false)
     setTimeout(() => {
@@ -49,6 +68,7 @@ function App() {
           <main>
             <ListadoGastos
               gastos = {gastos}
+              setGastoEditar={setGastoEditar}
             />
 
           </main>
@@ -67,6 +87,7 @@ function App() {
                     animarModal={animarModal}
                     setAnimarModal={setAnimarModal}
                     guardarGasto={guardarGasto}
+                    gastoEditar={gastoEditar}
                     />}
       </div>
 
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { LeadingActions,SwipeableList,SwipeableListItem,SwipeAction,TrailingActions } from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css'
+
 import { formatearFecha } from '../helpers'
+
 import IconoAhorro from '../img/icono_ahorro.svg'
 import IconoCasa from '../img/icono_casa.svg'
 import IconoComida from '../img/icono_comida.svg'
@@ -19,9 +23,34 @@ const diccionarioIconos = {
 
 }
 
-const Gasto = ({gasto}) => {
+const Gasto = ({gasto, setGastoEditar}) => {
     const {categoria, nombre, cantidad, id, fecha} = gasto
-  return (
+    
+    const leadingActions = () => (
+
+        <LeadingActions>
+            <SwipeAction onClick={() => setGastoEditar(gasto)}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => console.log('Eliminar')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
+
+    
+
+    return (
+    <SwipeableList>
+        <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+        >
     <div className='gasto sombra'>
         <div className='contenido-gasto'>
             <img 
@@ -45,6 +74,8 @@ const Gasto = ({gasto}) => {
         <p className='cantidad-gasto'>${cantidad}</p>
       
     </div>
+    </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
